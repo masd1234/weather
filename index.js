3f4a88460f84aadc9adbde1dd5f31fcbb43fe8b8
Marco Sanchez <masd_123@hotmail.com>
2021-05-27 23:02:54 -0300

    project deploy

---
+++ index.js
@@ -0,0 +1,68 @@
+const submitButtom = document.getElementById("submitButtom");
+const modalButtom = document.querySelector(".modal-buttom-close");
+const modalErrorDom = document.querySelector(".modal-error");
+
+const add = (data) => {
+  console.log(data);
+  let containerCity = document.createElement("div");
+  containerCity.classList.add("item-container");
+  const resultcontainerDom = document.querySelector(".result-container");
+
+  resultcontainerDom.appendChild(containerCity)(
+    (containerCity.innerHTML = `
+      <div class="icon-container">
+      <img class="icon" src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" >
+      <h1 id="city-name">${data.name}</h1>
+      </div>
+      <div class="data-container">
+      <p>Weather: ${data.weather[0].main}</p>
+      <p>Details: ${data.weather[0].description}</p>
+      <p>Temperature:  ${data.main.temp}°C</p>
+      <p>Longitud: ${data.coord.lon}</p>
+      <p>Latitud: ${data.coord.lat}</p>
+      </div>
+      `)
+  );
+};
+
+const fetchWeather = async () => {
+  let key = "62e6f90a3ac0fe0a0d4005ae5d3f14b9";
+  let inputCity = document.getElementById("city").value;
+
+  if (!inputCity) {
+    alert("please enter a city");
+    return;
+  } else {
+    await fetch(
+      `https:\\api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${key}`
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw modalErrorDom.classList.add("display");
+        }
+      })
+      .then((data) => {
+        add(data);
+      })
+      .catch((err) => console.log(err));
+    document.getElementById("city").value = "";
+  }
+};
+
+const closeModal = (e) => {
+  if (e.target.classList.contains("modal-buttom-close")) {
+    e.target.parentNode.classList.remove("display");
+  }
+};
+
+window.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    fetchWeather();
+  }
+});
+
+submitButtom.addEventListener("click", fetchWeather);
+modalButtom.addEventListener("click", closeModal);
+modalButtom.addEventListener("click", closeModal);
